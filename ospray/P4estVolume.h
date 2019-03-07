80ce049fc6370feb802b93daab4f0bd592cda3c8
Will Usher <willusher.life@gmail.com>
2019-03-07 15:42:48 -0700

    Add local copy and stashing of user ptr

--- ospray/P4estVolume.h
+++ ospray/P4estVolume.h
@@ -14,6 +14,7 @@
 #include <p4est_extended.h>
 #else
 #include <p8est_extended.h>
+#include <p8est_ospray.h>
 #endif
 
 
@@ -22,6 +23,8 @@ using namespace ospcommon;
 
 /*! abstract base class for any type of scalar volume sampler
 * we will eventually specialize this for bricktree further below
+* TODO: Don't think we really need this, maybe to combined with the hybrid
+* isosurface rendering in the future?
 */
 class ScalarVolumeSampler
 {
@@ -54,7 +57,6 @@ public:
 
   ScalarVolumeSampler *createSampler();
 
-protected:
   // The raw p4est data buffer passed to use through the API for the octree
   //ospray::Data *p4estTree;
 
@@ -63,6 +65,63 @@ protected:
   //p4est tree handler
   p4est_t            *p4est;
 };
+
+int pt_search_callback(p4est_t * p4est,
+                       p4est_topidx_t which_tree,
+                       p4est_quadrant_t * quadrant,
+                       p4est_locidx_t local_num,
+                       void *point){
+  //ASSUME that our point is a length 3 / length 2 array of doubles.
+  vec3f *pt = (vec3f*)point;
+  double lower_corner[3];
+  double upper_corner[3];
+
+  //obtain the upper and lower corner location of the octant/quadrant in world space
+	p4est_qcoord_t	int_len = P4EST_QUADRANT_LEN (quadrant->level); //lengh in integer coords
+
+  p4est_qcoord_to_vertex(p4est->connectivity,
+                         which_tree,
+                         quadrant->x,
+                         quadrant->y,
+#ifdef P4_TO_P8
+                         quadrant->z,
+#endif
+                         lower_corner);
+
+  p4est_qcoord_to_vertex(p4est->connectivity,
+                         which_tree,
+                         quadrant->x + int_len,
+                         quadrant->y + int_len,
+#ifdef P4_TO_P8
+                         quadrant->z + int_len,
+#endif
+                         upper_corner);
+
+  //assuming that the point is in world space, determine if the point is inside or outside the octant/quadrant
+	if ( pt->x < lower_corner[0] ||  pt->x > upper_corner[0]
+			|| pt->y < lower_corner[1] ||  pt->y > upper_corner[1]
+#ifdef P4_TO_P8
+			|| pt->z < lower_corner[2] ||  pt->z > upper_corner[2]
+#endif
+	) {
+		return 0;	//outside, tell p4est to terminate traversal
+	} else { //point may be contained in the octant/quadrant 
+    if(local_num > 0){ //reached a leaf
+
+      // Here, we would do bilinear interpolation of the data value
+      // Right now I am just storing the coordinates of the quadrant, because in
+      // our toy quadtree example there is no data
+      // BOLD ASSUMPTION: the user pointer points to the head of an array of doubles with length at least 3.
+      p4est_qcoord_t* out_array = (p4est_qcoord_t*)p4est->user_pointer;
+      out_array[0] = quadrant->x;
+      out_array[1] = quadrant->y;
+#ifdef P4_TO_P8
+      out_array[2] = quadrant->z;
+#endif
+    }
+    return 1; //tells p4est point may be contained in the octant/quadrant 
+  }
+}
 
 
 class P4estVolumeSampler : public ScalarVolumeSampler
@@ -106,8 +165,17 @@ public:
     
     //Recall that in the search_point_fn callback, we wrote our result into the stack variable at address p4est->user_pointer
     //return *(p4est->user_pointer); 
-
-    return 0.2f; //For test purposes only
+    p8est_t local = *p4estv->p4est;
+    double result[3] = {0.f};
+    local.user_pointer = result;
+    sc_array_t search_pt_array;
+    search_pt_array.elem_size = 3*sizeof(float); 
+    search_pt_array.elem_count = 1;
+    search_pt_array.array = (char*)&pos;
+    // TODO put the tree ID in here
+    p4est_ospray_search_local(&local, 0, 0, NULL, pt_search_callback, &search_pt_array);
+    PING;
+    return result[0];
   }
   
   virtual vec3f computeGradient(const vec3f &pos) const override
